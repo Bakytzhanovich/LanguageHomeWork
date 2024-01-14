e84f9934688ec1da32041abd1f68df3633d3e66a
Nursat <bakytzhanuli@internet.ru>
2024-01-14 09:07:29 +0600

    first commit

---
+++ OzinsheDemo/OzinsheDemo/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OzinsheDemo
+//
+//  Created by Нурсат Шохатбек on 10.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
